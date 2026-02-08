d993386e6211adb0ccd82b24db77441450663aaa
Ajay Gupta <ajayg913954@gmail.com>
2026-02-08 21:58:15 +0000

    New problem today i have done

---
+++ problem4.js
@@ -0,0 +1,12 @@
+// Problem : -  given a number "n" find the nth element of the fibonacci sequenece
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(6));
